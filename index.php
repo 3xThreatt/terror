28859c5306b3e1bee7de5e1d9e120e7f8caa7a44
3xThreatt <74078818+3xThreatt@users.noreply.github.com>
2024-03-05 13:38:00 +1300

    Update index.php

--- index.php
+++ index.php
@@ -311,7 +311,7 @@
     </div>
 
     <div id="footer">
-        <div class="marquee">لوريم إيبسوم هو نص دمجي هو لوريم إيبسوم هو نص دمجي هو لوريم إيبسوم هو نص دمجي هو</div>
+        <div class="marquee">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
     </div>
 
     <div class="btc-counter" id="btcCounter">Bitcoin Mined: <span id="btcValue">0</span> BTC</div>
